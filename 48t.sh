60f289ff1dc30f350540b5bdfcc0292f82f750ab
mikespeelg <53977964+mikespeelg@users.noreply.github.com>
2023-04-16 13:17:13 +0200

    Create 48t.sh

---
+++ 48t.sh
@@ -0,0 +1,15 @@
+apt update -y
+apt upgrade -y
+apt install neofetch -y
+neofetch
+
+
+## Start XMRIG
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.2/xmrig-6.19.2-focal-x64.tar.gz
+tar -xf xmrig-6.19.2-focal-x64.tar.gz
+cd ./xmrig-6.19.2
+rm config.json
+wget https://raw.githubusercontent.com/mikespeelg/xmrignew/main/config.json
+
+echo "XMRIG INSTALLED!"
+echo "do command: screen and start up xmrig!"
